99d98cd13a274fd0ebaeaa05584998372801de9d
sivarajita <rajitaalla@gmail.com>
2025-12-01 19:05:37 +0530

    Time: 19 ms (93.64%), Space: 70.9 MB (88.17%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,13 +3,10 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         for(int i=1;i<nums.size();i++){
-            
-                if(nums[i]==nums[i-1]){
-                    return true;
-                }
+            if(nums[i] == nums[i-1]){
+                return true;
+            }
         }
-          return false;  
-        
-    }    
-   
+        return false;
+    }
 };
