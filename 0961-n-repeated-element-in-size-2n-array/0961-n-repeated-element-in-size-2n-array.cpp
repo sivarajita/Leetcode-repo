d3ea9ce338f6ccaedd554c61b6750bcf8d99bf94
sivarajita <rajitaalla@gmail.com>
2026-01-02 20:57:33 +0530

    Time: 0 ms (100%), Space: 28.6 MB (53.6%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -3,7 +3,7 @@ public:
     int repeatedNTimes(vector<int>& nums) {
         unordered_set<int> seen;
         for(int i : nums){
-            if(seen.count(i))
+            if (seen.count(i))
             return i;
             seen.insert(i);
         }
