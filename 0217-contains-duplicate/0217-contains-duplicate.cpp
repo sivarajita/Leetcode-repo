4d51443c4e1004d3d54fc7d8152e3e7ae424c147
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-05-17 20:09:12 +0530

    Time: 20 ms (92.58%), Space: 71.1 MB (80.61%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,10 +3,11 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         for(int i=1;i<nums.size();i++){
-            if(nums[i] == nums[i-1]){
+            if(nums[i]==nums[i-1]){
                 return true;
             }
         }
         return false;
-    }
+
+        }
 };
