fdd8fded86930291e6a4e8c8a1c931d6be0b4c9f
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-06-05 10:47:17 +0530

    Time: 0 ms (100%), Space: 13.4 MB (43.79%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n=matrix.size();//row
-        int m=matrix[0].size();//column
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]==target){
-                    return true;
-                }
+     int n=matrix.size();
+     int m=matrix[0].size();
+     for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(matrix[i][j]==target){
+                return true;
             }
         }
-        return false;
+     }   
+     return false;
     }
 };
