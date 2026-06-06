c4592d524a6d939803f8b3eebfcc3b1ed9fd2a47
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-06-06 20:30:42 +0530

    Time: 0 ms (100%), Space: 15 MB (81.37%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -4,14 +4,15 @@ public:
         int n=nums.size();
         int leftsum=0;
         int rightsum=0;
-        for(int num : nums){
+        for(int num:nums){
             rightsum+=num;
         }
         for(int i=0;i<n;i++){
             int val=nums[i];
             rightsum-=val;
-             nums[i]=abs(leftsum-rightsum);
+            nums[i] = abs(leftsum - rightsum);
             leftsum+=val;
+
         }
         return nums;
     }
