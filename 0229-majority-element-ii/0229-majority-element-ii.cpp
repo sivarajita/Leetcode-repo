de59794a77b28fee792fcdcbdcfaaed30912c9f3
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-06-06 11:44:09 +0530

    Time: 928 ms (5.19%), Space: 23.5 MB (96.71%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-     int n=nums.size();
-     vector<int> result;
-     for(int i=0;i<n;i++){
-        if(result.size()==0 || result[0]!=nums[i]){
-            int count=0;
-        
-     for(int j=0;j<n;j++){
-        if(nums[i]==nums[j]){
-            count++;
+        int n=nums.size();
+        vector<int> result;
+        for(int i=0;i<n;i++){
+            if(result.size()==0 || result[0]!=nums[i]){
+                int count=0;
+                for(int j=0;j<n;j++){
+                    if(nums[i]==nums[j]){
+                        count++;
+                    }}
+                    if(count>(n/3)){
+                        result.push_back(nums[i]);
+                    }
+                    if(result.size()==2){
+                        break;
+                    }
+            } 
         }
-        }
-        if(count>(n/3)){
-            result.push_back(nums[i]);
-        }
-        if(result.size()==2){
-            break;
-        }
-        }
-     }
-     return result;
-
+        return result;
     }
 };
