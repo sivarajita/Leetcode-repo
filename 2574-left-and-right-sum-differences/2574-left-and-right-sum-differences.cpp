7b263779685c89f47d030c76dafd596a5a38e5c9
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-06-06 20:16:06 +0530

    Time: 0 ms (100%), Space: 15 MB (81.37%) - LeetHub

---
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        int n=nums.size();
+        int leftsum=0;
+        int rightsum=0;
+        for(int num : nums){
+            rightsum+=num;
+        }
+        for(int i=0;i<n;i++){
+            int val=nums[i];
+            rightsum-=val;
+             nums[i]=abs(leftsum-rightsum);
+            leftsum+=val;
+        }
+        return nums;
+    }
+};
