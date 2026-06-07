4b9ece601a61f3a5e3369026126e24d615cb043f
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-06-07 11:07:31 +0530

    Time: 35 ms (34.6%), Space: 14 MB (94.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-      int n=nums.size();
-      vector<int> result;
-      for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-        if(nums[i]+nums[j]==target){
-            return {i,j};
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
+                }
+            }
         }
-        }
-      }  
-      return {};
+        return {};
     }
 };
