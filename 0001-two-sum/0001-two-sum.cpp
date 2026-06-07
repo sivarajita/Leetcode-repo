c4c1f260a713a3278aefd2d2fca2a57e778454ad
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-06-07 11:04:32 +0530

    Time: 35 ms (34.6%), Space: 14.2 MB (68.6%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> pairIdx;
-        for (int i = 0; i < nums.size(); ++i) {
-            int num = nums[i];
-            if (pairIdx.find(target - num) != pairIdx.end()) {
-                return {i, pairIdx[target - num]};
-            }
-            pairIdx[num] = i;
+      int n=nums.size();
+      vector<int> result;
+      for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+        if(nums[i]+nums[j]==target){
+            return {i,j};
         }
-
-        return {};        
+        }
+      }  
+      return {};
     }
 };
