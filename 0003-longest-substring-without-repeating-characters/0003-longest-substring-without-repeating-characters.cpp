f7e15fcb1a83dfb03346a8488052264db131d43f
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-06-08 11:37:47 +0530

    Time: 59 ms (7.73%), Space: 126.3 MB (5.2%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n=s.size();
-        int maxLen=0;
-        for(int i=0;i<n;i++){
-            vector<int> hash(256,0);
-            for(int j=i;j<n;j++){
-                if(hash[s[j]]==1){
-                    break;
-                }
-                    hash[s[j]]=1;
-                    int len=j-i+1;
-                    maxLen=max(maxLen,len);
-                }
+       int n=s.size();
+       int maxlen=0;
+       for(int i=0;i<n;i++){
+        vector<int> hash(256,0);
+        for(int j=i;j<n;j++){
+            if(hash[s[j]]==1){
+                break;
+            }
+            hash[s[j]]=1;
+            int len=j-i+1;
+            maxlen=max(maxlen,len);
         }
-        return maxLen;
+       } 
+       return maxlen;
     }
 };
