02d51211ea72cda84046dc36b73b18f8cfd1bf66
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-06-16 12:29:55 +0530

    Time: 7 ms (35.61%), Space: 39.7 MB (87.5%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result="";
+        for(char c:s){
+            if(c>='a' && c<='z'){
+                result.push_back(c);
+            }
+            else if(c=='*'){
+               if(!result.empty()){
+                result.pop_back();
+               }
+            }
+            else if(c=='#'){
+                result+=result;
+            }
+            else if(c=='%'){
+                reverse(result.begin(),result.end());
+            }
+        }
+        return result;
+    }
+};
