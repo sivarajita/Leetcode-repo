da92b5b070a7cd14de9cc44871725d0c4a2f253c
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-06-19 21:54:18 +0530

    Time: 0 ms (100%), Space: 10.8 MB (44.68%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int maxa=0;
+        int curr=0;
+        for(int i=0;i<gain.size();i++){
+            curr+=gain[i];
+            maxa=max(maxa,curr);
+        }
+        return maxa;
+    }
+};
