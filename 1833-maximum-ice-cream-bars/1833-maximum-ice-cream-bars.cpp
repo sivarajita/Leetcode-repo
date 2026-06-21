a024ee2b41ebd0a63becc6a975f082b3e3424da2
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-06-21 13:22:10 +0530

    Time: 5 ms (90.13%), Space: 84.4 MB (19.23%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        int max_cost=0;
-        for(int cost:costs){
-            max_cost=max(max_cost,cost);
+       int max_cost=0;
+       for(int cost:costs){
+        max_cost=max(max_cost,cost);
        }
        vector<int> freq(max_cost+1,0);
        for(int cost:costs){
         freq[cost]++;
        }
-       int icecreamcount=0;
-       for(int cost=1;cost<=max_cost;cost++){
-        if(freq[cost]==0)
-        continue;
-        if(coins<cost)
-        break;
-        int countTobuy=min(freq[cost],coins/cost);
-        icecreamcount+=countTobuy;
-        coins-=countTobuy*cost;
-       }
-       return icecreamcount;
+        
+     int icecreamcount=0;
+          for(int cost=1;cost<=max_cost;cost++){
+            if(freq[cost]==0)
+            continue;
+            if(coins<cost)
+            break;
+            int counttobuy=min(freq[cost],coins/cost);
+            icecreamcount+=counttobuy;
+            coins-=counttobuy*cost;
+          }
+          return icecreamcount;
     }
 };
