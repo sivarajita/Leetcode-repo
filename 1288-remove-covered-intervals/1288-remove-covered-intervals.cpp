6856fdd94adbf8adecfcad81675d63935b33ca50
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-07-06 19:42:45 +0530

    Time: 2 ms (46.09%), Space: 15.1 MB (82.12%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        std::sort(intervals.begin(),intervals.end(),[](const std::vector<int>& a, const std::vector<int>& b){
+            if(a[0]==b[0]){
+                return a[1]>b[1];
+
+            }
+            return a[0]<b[0];
+        });
+        int remainingcount=0;
+        int maxend=0;
+        for(const auto& interval : intervals){
+            if(interval[1]>maxend){
+                remainingcount++;
+                maxend=interval[1];
+            }
+        }
+        return remainingcount;
+    }
+};
