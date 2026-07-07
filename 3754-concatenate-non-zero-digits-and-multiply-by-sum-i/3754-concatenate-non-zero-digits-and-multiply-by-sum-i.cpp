c5a0be6d1421cb86399554b172b4e17a37304c5c
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-07-07 19:47:17 +0530

    Time: 0 ms (100%), Space: 8.8 MB (98.81%) - LeetHub

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        if(n==0)
+        return 0;
+        long long x=0;
+        long long digit_sum=0;
+        long long place_value=1;
+        while(n>0){
+            int digit=n%10;
+            if(digit!=0){
+                digit_sum+=digit;
+                x+=digit*place_value;
+                place_value*=10;
+            }
+            n/=10;
+        }
+        return x*digit_sum;
+    }
+};
