61e4670f8764cdac69afc8fbb8970f713e32c415
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-07-07 19:56:02 +0530

    Time: 0 ms (100%), Space: 8.8 MB (89.09%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -6,7 +6,7 @@ public:
         long long x=0;
         long long digit_sum=0;
         long long place_value=1;
-        while(n>0){
+         while(n>0){
             int digit=n%10;
             if(digit!=0){
                 digit_sum+=digit;
@@ -14,7 +14,7 @@ public:
                 place_value*=10;
             }
             n/=10;
-        }
-        return x*digit_sum;
+         }
+         return x*digit_sum;
     }
 };
