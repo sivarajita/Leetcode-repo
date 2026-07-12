56957d356ee063f6a564a7849dbf41472c14f25a
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-07-12 11:54:04 +0530

    Time: 39 ms (55.31%), Space: 43.9 MB (55.31%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-       vector<int> sorted_arr=arr;
-       sort(sorted_arr.begin(),sorted_arr.end());
-       unordered_map<int, int> rank_map;
-       int rank=1;
-       for(int num : sorted_arr){
+      vector<int> sorted_arr=arr;
+      sort(sorted_arr.begin(),sorted_arr.end());
+      unordered_map<int, int> rank_map;
+      int rank=1;
+      for(int num : sorted_arr){
         if(rank_map.find(num)==rank_map.end()){
             rank_map[num]=rank;
             rank++;
         }
-       }
-       vector<int> result;
-       result.reserve(arr.size());
-       for(int num : arr){
+      }
+      vector<int> result;
+      result.reserve(arr.size());
+      for(int num:arr){
         result.push_back(rank_map[num]);
-       }
-       return result;
+      }
+      return result;
+
+      
     }
 };
