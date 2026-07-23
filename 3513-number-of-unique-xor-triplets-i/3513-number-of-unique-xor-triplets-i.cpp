08e869c2cb70e56eb06b714e357af8fd27d4d22e
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-07-23 21:03:49 +0530

    Time: 0 ms (100%), Space: 198.8 MB (85%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+      int n = nums.size();
+        
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        
+        // Find the smallest power of 2 strictly greater than n
+        int power = 1;
+        while (power <= n) {
+            power <<= 1;
+        }
+        
+        return power;  
+    }
+};
