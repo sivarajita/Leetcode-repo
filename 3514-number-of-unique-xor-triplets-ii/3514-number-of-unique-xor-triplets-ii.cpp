f1dcb67dae7fd89944f65239ea42d585d8c9d755
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-07-24 17:09:05 +0530

    Time: 513 ms (75.84%), Space: 43.1 MB (66.85%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-       unordered_set<int> unique_nums_set(nums.begin(),nums.end());
-       vector<int> unique_nums(unique_nums_set.begin(),unique_nums_set.end());
-       const int MAX_VAL=2048;
-       vector<bool> current_reachable(MAX_VAL,false);
-       current_reachable[0]=true;
-       for(int step=0;step<3;step++){
-        vector<bool> next_reachable(MAX_VAL,false);
-        for(int val=0;val<MAX_VAL;val++){
-            if(current_reachable[val]){
-                for(int num:unique_nums){
-                    next_reachable[val^num]=true;
+        unordered_set<int> unique_nums_set(nums.begin(), nums.end());
+        std::vector<int> unique_nums(unique_nums_set.begin(), unique_nums_set.end());
+
+        // Maximum possible value for elements <= 1500 is < 2048
+        const int MAX_VAL = 2048;
+        std::vector<bool> current_reachable(MAX_VAL, false);
+        current_reachable[0] = true;
+
+        // Step 2: Expand reachable XOR values for 3 rounds (3 picks)
+        for (int step = 0; step < 3; ++step) {
+            std::vector<bool> next_reachable(MAX_VAL, false);
+            
+            for (int val = 0; val < MAX_VAL; ++val) {
+                if (current_reachable[val]) {
+                    for (int num : unique_nums) {
+                        next_reachable[val ^ num] = true;
+                    }
                 }
             }
+            current_reachable = std::move(next_reachable);
         }
-        current_reachable=move(next_reachable);
-       } 
-       int count=0;
-       for(int val=0;val<MAX_VAL;val++){
-        if(current_reachable[val]){
-            count++;
+
+        // Step 3: Count the total unique XOR triplet values
+        int count = 0;
+        for (int val = 0; val < MAX_VAL; ++val) {
+            if (current_reachable[val]) {
+                count++;
+            }
         }
-       }
-       return count;
+
+        return count;
     }
 };
