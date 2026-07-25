3c385dc588f1aca2b3a379ef0f6030c15f66b0f2
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-07-25 17:59:06 +0530

    Time: 0 ms (100%), Space: 8.7 MB (59.34%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        vector<int> digits;
-        while(n>0){
-            digits.push_back(n%10);
-            n/=10;
-        }
-        int max1=0,max2=0;
-        for(int d : digits){
-            if(d>max1){
-                max2=max1;
-                max1=d;
+       int max1 = 0;
+        int max2 = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            n /= 10;
+
+            if (digit >= max1) {
+                max2 = max1;
+                max1 = digit;
+            } else if (digit > max2) {
+                max2 = digit;
             }
-            else if(d>max2){
-                max2=d;
-            }
         }
-        return max1*max2;
+
+        return max1 * max2; 
     }
 };
