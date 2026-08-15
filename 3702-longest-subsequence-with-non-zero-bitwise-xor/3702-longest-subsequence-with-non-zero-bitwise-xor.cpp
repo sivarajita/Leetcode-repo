4cde001239f3765aa0a7926e4bf98b46e6a7813a
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-08-15 12:12:33 +0530

    Time: 0 ms (100%), Space: 171.1 MB (88.49%) - LeetHub

--- 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int longestSubsequence(vector<int>& nums) {
-        int total_xor=0;
-        int non_zero=false;
-        for(int x:nums){
-            total_xor^=x;
-            if(x!=0){
-                non_zero=true;
-            }
-        }
-        if(!non_zero)
-        return 0;
-        if(total_xor!=0)
-        return nums.size();
-        return nums.size()-1;
-    }
+        int txor=0;
+        bool non_zero=false;
+ for(int x:nums){
+    txor^=x;
+
+    if(x!=0)
+    non_zero=true;
+ }
+ if(!non_zero)
+ return 0;
+ if(txor!=0)
+ return nums.size();
+ return nums.size()-1;
+}
 };
