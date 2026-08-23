268934f0c80ede6f58e9ad61ff35460f41bc417a
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-08-23 11:28:20 +0530

    Time: 8 ms (12.59%), Space: 14.1 MB (33.57%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.length();
+        double sum_diff = 0; // left_sum - right_sum
+        
+        for (int i = 0; i < n; ++i) {
+            double val = (num[i] == '?') ? 4.5 : (num[i] - '0');
+            if (i < n / 2) {
+                sum_diff += val;
+            } else {
+                sum_diff -= val;
+            }
+        }
+        
+        // Bob wins if and only if the balance is exactly 0
+        return sum_diff != 0.0;
+    }
+};
