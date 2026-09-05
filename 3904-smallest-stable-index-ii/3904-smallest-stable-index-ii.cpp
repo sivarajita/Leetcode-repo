00ae64a903ff6bb8551962531a3bdd0afc5acefa
ALLA SIVA RAJITA <rajitaalla@gmail.com>
2026-09-05 19:20:59 +0530

    Time: 4 ms (89.3%), Space: 202.7 MB (76.32%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int> suffMin(n);
+        suffMin[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+        suffMin[i]=min(nums[i],suffMin[i+1]);
+        }
+        int prefMax=nums[0];
+        for(int i=0;i<n;i++){
+            prefMax=max(prefMax,nums[i]);
+            if(prefMax-suffMin[i]<=k){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
